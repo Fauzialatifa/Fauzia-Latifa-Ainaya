21815186dd1415e7223c3874e47d27d6c048bccc
Fauzialatifa <54698713+Fauzialatifa@users.noreply.github.com>
2019-09-12 11:54:30 +0700

    Add files via upload

---
+++ Quiz41_Fauzia Latifa Ainaya_1201184195.py
@@ -0,0 +1,6 @@
+ListMahasiswa = ["Anton", "Budi", "Dori", "Huda"]
+print(ListMahasiswa)
+print(ListMahasiswa[0]);
+print(ListMahasiswa[1]);
+print(ListMahasiswa[2]);
+print(ListMahasiswa[3])
---
+++ Quiz42_Fauzia Latifa Ainaya_1201184195.py
@@ -0,0 +1,7 @@
+fruits = ["Nanas", "Apel", "Pepaya", "Kentang"]
+length = len(fruits)
+i = 0
+
+while i < length :
+    print(fruits[i])
+    i += 1
